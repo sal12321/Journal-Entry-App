288d400cf79728a7d723c781236a6f0833c3393c
salim <aaqibalam290@gmail.com>
2025-10-18 12:09:24 +0530

    retrieving data from cloud

--- src/main/java/com/salAce/journalApp/cache/LabsAppCache.java
+++ src/main/java/com/salAce/journalApp/cache/LabsAppCache.java
@@ -3,6 +3,7 @@ package com.salAce.journalApp.cache;
 import com.salAce.journalApp.entity.ConfigJournalAppEntity;
 import com.salAce.journalApp.repo.ConfigJournalAppRepo;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.util.Map;
 
 ///  making this class as a component as this is made to store the config from the database and immediately as we are using the PostConstruct annotation
 /// trying to cash the data to use in another class
+@Slf4j
 @Component
 public class LabsAppCache {
 
@@ -27,7 +29,18 @@ public class LabsAppCache {
         // find all the entity from db and store them in HashMap
         for(ConfigJournalAppEntity configJournalAppEntity : all ){
             // in the first round it will store the first item in db i.e {api} and then {api_key}
-            APP_CACHE.put(configJournalAppEntity.getKey() , configJournalAppEntity.getValue()) ;
+            if(configJournalAppEntity.getService().equals("elevenlabs")){
+                APP_CACHE.put("key" , configJournalAppEntity.getKey()) ;
+                APP_CACHE.put("url" , configJournalAppEntity.getUrl()) ;
+                APP_CACHE.put("service" , configJournalAppEntity.getService()) ;
+                log.info("SERVICE " + configJournalAppEntity.getService()) ;
+                log.info("URL " + configJournalAppEntity.getUrl()) ;
+                log.info("KEY " + configJournalAppEntity.getKey()) ;
+
+                return;
+            }
+
+
         }
 
 
