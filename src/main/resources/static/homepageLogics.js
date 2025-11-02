17883dadc10ac295143521c28d78087840cbfa18
salim <aaqibalam290@gmail.com>
2025-11-02 12:18:20 +0530

    Fix API URL for same-origin deployment
    # Conflicts:
    #	src/main/resources/static/homepageLogics.js

--- src/main/resources/static/homepageLogics.js
+++ src/main/resources/static/homepageLogics.js
@@ -1,6 +1,8 @@
 //    const API_URL = 'https://journal-entry-app-production.up.railway.app/';
-    const API_URL = 'https://journal-entry-app-production.up.railway.app/';
+//    const API_URL = 'https://journal-entry-app-production.up.railway.app';
 //const API_URL = 'http://localhost:8080';
+const API_URL = "";
+
 let authToken = '';
 let currentUser = {};
 console.log("JS file loaded!");
