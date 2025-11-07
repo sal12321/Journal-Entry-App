3352dc8437153312a73f1b148d9cade5624da862
salim <aaqibalam290@gmail.com>
2025-11-07 15:50:19 +0530

    adding production url

--- src/main/resources/static/homepageLogics.js
+++ src/main/resources/static/homepageLogics.js
@@ -1,7 +1,7 @@
 //    const API_URL = 'https://journal-entry-app-production.up.railway.app/';
 //    const API_URL = 'https://journal-entry-app-production.up.railway.app';
 //const API_URL = 'http://localhost:8080';
-const API_URL = "https://journal-entry-app-production.up.railway.app/";
+const API_URL = "https://journal-entry-app-production.up.railway.app";
 
 let authToken = '';
 let currentUser = {};
