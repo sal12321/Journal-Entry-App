faf24d7060ede671a7283401ec0e5bec564cdd10
salim <aaqibalam290@gmail.com>
2025-11-07 15:34:01 +0530

    api url as localhost:8080

--- src/main/resources/static/homepageLogics.js
+++ src/main/resources/static/homepageLogics.js
@@ -1,6 +1,6 @@
-    const API_URL = 'https://journal-entry-app-production.up.railway.app/';
+//    const API_URL = 'https://journal-entry-app-production.up.railway.app/';
 //    const API_URL = 'https://journal-entry-app-production.up.railway.app';
-//const API_URL = 'http://localhost:8080';
+const API_URL = 'http://localhost:8080';
 //const API_URL = "";
 
 let authToken = '';
