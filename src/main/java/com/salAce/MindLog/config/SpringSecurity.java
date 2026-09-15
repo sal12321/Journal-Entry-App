a232ee55c4a2bf83106ac0be98b42d48427238ba
sal12321 <aaqibalam290@gmail.com>
2026-09-15 22:17:18 +0530

    resolving cors

--- src/main/java/com/salAce/MindLog/config/SpringSecurity.java
+++ src/main/java/com/salAce/MindLog/config/SpringSecurity.java
@@ -255,7 +255,8 @@ public class SpringSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+//        for local devs and production
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://mindlog-jurnaling-frontend-using-ne.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
