c8f5556a42fe98af0a3beb55b31b624eebd3a495
sal12321 <aaqibalam290@gmail.com>
2026-09-17 01:10:02 +0530

    adding api for admin edit, delete

--- src/main/java/com/salAce/MindLog/controller/AdminController.java
+++ src/main/java/com/salAce/MindLog/controller/AdminController.java
@@ -1,12 +1,15 @@
 package com.salAce.MindLog.controller;
 
 import com.salAce.MindLog.entity.CreateAdminDTO;
+import com.salAce.MindLog.entity.User;
 import com.salAce.MindLog.entity.UserDetailsVisibleToAdmin;
 import com.salAce.MindLog.service.UserEntryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.flogger.Flogger;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +26,9 @@ import java.util.List;
 public class AdminController {
 
     @Autowired
+
   private  UserEntryService userEntryService ;
+
 
 
     @GetMapping("/all-users")
@@ -50,5 +55,31 @@ public class AdminController {
         }
 return new ResponseEntity<>(HttpStatus.BAD_REQUEST) ;
     }
+
+    @PutMapping("/edit")
+    public ResponseEntity<?> updateAdmin(@RequestBody User user){
+        Long count = 0L;
+        try{
+             count = userEntryService.updateAdmin(user);
+
+            return new ResponseEntity<Long>(count, HttpStatus.ACCEPTED);
+        } catch(Exception e){
+            log.info(e.getMessage());
+        }
+        return new ResponseEntity<Long>(count, HttpStatus.NOT_FOUND);
+
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteAdmin(@RequestBody User user){
+        try {
+            Long count = userEntryService.deleteAdmin(user.getEmail());
+            return new ResponseEntity<>(count, HttpStatus.OK);
+        } catch(Exception e){
+            log.info(e.getMessage());
+            return new ResponseEntity<>(0, HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
