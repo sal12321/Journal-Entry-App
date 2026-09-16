81aecb9cfb1d78194123d744e9de15e7ea38326f
sal12321 <aaqibalam290@gmail.com>
2026-09-17 01:11:08 +0530

    adding repo method with search annotation

--- src/main/java/com/salAce/MindLog/repo/UserEntryRepo.java
+++ src/main/java/com/salAce/MindLog/repo/UserEntryRepo.java
@@ -4,8 +4,10 @@ import com.salAce.MindLog.entity.UserDetailsVisibleToAdmin;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserEntryRepo extends MongoRepository<User , ObjectId>{
@@ -14,6 +16,17 @@ public interface UserEntryRepo extends MongoRepository<User , ObjectId>{
 
     @Query(value = "{}", fields = "{ 'userName' : 1, 'email' : 1, 'sentimentAnalysis' : 1, 'roles' : 1 }")
     List<UserDetailsVisibleToAdmin> findUserDetailsVisibleToAdmin();
+
+
+    Long deleteByEmail(String email);
+
+
+    @Query("{ 'email' : ?0 }")
+    @Update("{ '$set' : { 'userName' : ?1, 'sentimentAnalysis' : ?2 } }")
+    Long updateUserDetailsByEmail(String email, String userName, boolean sentimentAnalysis);
+
+
+
 
 
 }
